a359cff22da6e4a9c5432e81d9efa75ee6c9ad8b
DorryB <dorrybjorndalen@gmail.com>
2024-07-02 17:05:34 +0400

    2.06

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,12 @@ export default function Header(){
         <header>
             <div>
                 <span className={styles.logo}>Курсы SMM</span>
+                <ul className={styles.nav}>
+                    <li> О нас</li>
+                    <li>Контакты</li>
+                    <li> Личный кабинет </li>
+
+                </ul>
             </div>
             <div className={styles.presentation}></div>
         </header>
