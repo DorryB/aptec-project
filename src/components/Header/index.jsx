b50bb5a6b4b91ce2c686bc0b4ce0025c3fb2af13
DorryB <dorrybjorndalen@gmail.com>
2024-07-03 15:16:59 +0400

    3.07

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.scss';
+import { FaHandPeace } from 'react-icons/fa';
+import { SlBasket } from 'react-icons/sl';
 
-export default function Header(){
-    return (
-        <header>
-            <div>
-                <span className={styles.logo}>Курсы SMM</span>
-                <ul className={styles.nav}>
-                    <li> О нас</li>
-                    <li>Контакты</li>
-                    <li> Личный кабинет </li>
-
-                </ul>
-            </div>
-            <div className={styles.presentation}></div>
-        </header>
-    );
+export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
+  return (
+    <header>
+      <div>
+        <FaHandPeace className={styles.icon} />
+        <span className={styles.logo}>Курсы SMM</span>
+        <ul className={styles.nav}>
+          <li> О нас</li>
+          <li>Контакты</li>
+          <li> Личный кабинет </li>
+        </ul>
+        <SlBasket
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`${styles.shopCartButton} ${
+            cartOpen ? styles.active : ''
+          }`}
+        />
+      </div>
+      {cartOpen && <div className={styles.shopCart}></div>}
+      <div className={styles.presentation}></div>
+    </header>
+  );
 }
---
+++ src/components/Orders/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Order.module.scss";
+
+export default function Orders(){
+    return(
+        <></>
+    );
+}
